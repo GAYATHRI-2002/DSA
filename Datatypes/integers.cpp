faa67cebd264d3f0db46da92ae7f5a22b79becf9
Gayathri J Nair <gayathrij5003@gmail.com>
2026-06-09 14:02:26 +0530

    datatype concept

---
+++ Datatypes/integers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int myNum = 2000;
+    cout<< myNum <<endl;
+    return 0;
+}
---
+++ Datatypes/strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 100;
+    cout<<"Number is "<<number<<endl;
+    float floatnumber = 1.3;
+    cout<<"float Number is "<<floatnumber<<endl;
+
+    double dnum = 1.3243;
+    cout<<"Double Number is "<<dnum<<endl;
+
+    bool bnum = true;
+    cout<<"Boolean  is "<<bnum<<endl;
+
+    char a = 'B';
+    cout<<"Character is "<<a<<endl;
+
+    string mystring = "This is a string";
+    cout<< mystring <<endl;
+    return 0;
+}
