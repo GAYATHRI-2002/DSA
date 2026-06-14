1c3fe2e04254e138c9d64022abeed2e11d8be58b
Gayathri J Nair <gayathrij5003@gmail.com>
2026-06-14 19:32:12 +0530

    array  concept

---
+++ Arrays/demo1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    string states[] = {"Kerala", "Tamilnadu", "Assam", "Delhi", "Mumbai"};
+    cout<<states[4]<<endl;
+    int number[] = {1,3,45,6};
+    cout<< number[0] + number[1]<<endl;
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars[0] = "Opel";
+    cout << cars[0]<<endl;
+
+    string text1 = "Hello";
+    cout<<text1<<endl;
+}
